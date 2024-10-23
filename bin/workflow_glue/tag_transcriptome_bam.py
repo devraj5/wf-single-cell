77f83f8232e0ac0873b2e87951f4a7a72cdbfb5c
Dev Raj Bhattarai <drbhatta@login004.cm.cluster>
2024-10-23 16:31:29 -0500

    added tag_transcriptome_bam.py script to get tagged_tr.bam file

--- bin/workflow_glue/tag_transcriptome_bam.py
+++ bin/workflow_glue/tag_transcriptome_bam.py
@@ -1,61 +1,62 @@
 #!/usr/bin/env python
-import argparse
+"""Tag transcriptome-aligned BAM file with information from read_summary.tsv"""
+
 import pysam
-from collections import namedtuple
-import logging
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-TagData = namedtuple('TagData', ['cb', 'cr', 'cy', 'ub', 'ur', 'uy', 'gn', 'tr'])
-
-def read_tag_file(filename):
-    read_tags = {}
-    with open(filename, "r") as f:
-        next(f)  # Skip header
-        for line in f:
-            cols = line.strip().split("\t")
-            read_id = cols[0]
-            read_tags[read_id] = TagData(*cols[2:10])
-    return read_tags
+import csv
+import argparse
+from .util import get_named_logger, wf_parser  # noqa: ABS101
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('input_bam', help='Input BAM file')
-    parser.add_argument('output_bam', help='Output BAM file')
-    parser.add_argument('reads_summary', help='TSV file with read tags')
-    parser.add_argument('--threads', type=int, default=1, help='Number of threads to use')
+    parser = wf_parser("tag_transcriptome_bam")
+    parser.add_argument("input_bam", help="Input transcriptome-aligned BAM file")
+    parser.add_argument("output_bam", help="Output tagged BAM file")
+    parser.add_argument("read_summary", help="read_summary.tsv file")
+    parser.add_argument("--threads", type=int, default=1, help="Number of threads to use")
     args = parser.parse_args()
 
-    read_tags = read_tag_file(args.reads_summary)
-    logger.info(f"Loaded {len(read_tags)} read tags")
+    logger = get_named_logger("TagTranscriptomeBAM")
+    
+    read_info = load_read_summary(args.read_summary)
+    tag_bam(args.input_bam, args.output_bam, read_info, args.threads, logger)
 
-    with pysam.AlignmentFile(args.input_bam, "rb", threads=args.threads) as bamfile, \
-         pysam.AlignmentFile(args.output_bam, "wb", template=bamfile, threads=args.threads) as output_bam:
+def load_read_summary(file_path):
+    read_info = {}
+    with open(file_path, 'r') as f:
+        reader = csv.DictReader(f, delimiter='\t')
+        for row in reader:
+            read_info[row['read_id']] = row
+    return read_info
+
+def tag_bam(input_bam, output_bam, read_info, threads, logger):
+    with pysam.AlignmentFile(input_bam, "rb", threads=threads) as in_bam, \
+         pysam.AlignmentFile(output_bam, "wb", template=in_bam, threads=threads) as out_bam:
         
         total_reads = 0
         tagged_reads = 0
+        
+        for read in in_bam:
+            total_reads += 1
+            read_id = read.query_name
+            if read_id in read_info:
+                tagged_reads += 1
+                info = read_info[read_id]
+                read.set_tag("CB", info['corrected_barcode'], "Z")
+                read.set_tag("CR", info['uncorrected_barcode'], "Z")
+                read.set_tag("CY", info['quality_barcode'], "Z")
+                read.set_tag("UB", info['corrected_umi'], "Z")
+                read.set_tag("UR", info['uncorrected_umi'], "Z")
+                read.set_tag("UY", info['quality_umi'], "Z")
+                read.set_tag("GN", info['gene'], "Z")
+                read.set_tag("TR", info['transcript'], "Z")
+                
+                # Add transcriptome-specific tags
+                read.set_tag("TS", read.reference_name, "Z")  # Transcript name
+                read.set_tag("TL", read.reference_length, "i")  # Transcript length
+            
+            out_bam.write(read)
 
-        for ref in bamfile.references:
-            logger.info(f"Processing reference: {ref}")
-            for read in bamfile.fetch(ref):
-                total_reads += 1
-                read_id = read.query_name
-                if read_id in read_tags:
-                    tagged_reads += 1
-                    tags = read_tags[read_id]
-                    read.set_tag("CB", tags.cb)
-                    read.set_tag("CR", tags.cr)
-                    read.set_tag("CY", tags.cy)
-                    read.set_tag("UB", tags.ub)
-                    read.set_tag("UR", tags.ur)
-                    read.set_tag("UY", tags.uy)
-                    read.set_tag("GN", tags.gn)
-                    read.set_tag("TR", tags.tr)
-                output_bam.write(read)
-
-        logger.info(f"Processed {total_reads} reads")
-        logger.info(f"Tagged {tagged_reads} reads ({tagged_reads/total_reads:.2%})")
+    logger.info(f"Total reads processed: {total_reads}")
+    logger.info(f"Reads tagged: {tagged_reads} ({tagged_reads/total_reads:.2%})")
 
 if __name__ == "__main__":
     main()
