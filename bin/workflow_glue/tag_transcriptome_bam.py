10cd9c850915abb3147e100bb72b543f2f2a350b
Dev Raj Bhattarai <drbhatta@login004.cm.cluster>
2024-10-29 09:54:33 -0500

    added tag_transcriptome_bam.py for tagging and UMI deduplication

--- bin/workflow_glue/tag_transcriptome_bam.py
+++ bin/workflow_glue/tag_transcriptome_bam.py
@@ -1,7 +1,10 @@
 """Tag transcriptome-aligned BAM file with information from read_summary.tsv"""
 
 import pysam
-import csv
+import pandas as pd
+import collections
+import itertools
+from umi_tools import UMIClusterer
 from .util import get_named_logger, wf_parser
 
 def argparser():
@@ -12,66 +15,106 @@ def argparser():
     parser.add_argument("--threads", type=int, default=1, help="Number of threads to use")
     return parser
 
-def main(args):
-    logger = get_named_logger("TagTranscriptomeBAM")
+def get_adj_list_directional_lev(self, umis, counts, threshold=2):
+    """Use Levenshtein distance for UMIclustering instead of hamming."""
+    from editdistance import eval as edit_distance
+    adj_list = {umi: [] for umi in umis}
+    for umi1, umi2 in itertools.combinations(umis, 2):
+        if edit_distance(umi1, umi2) <= threshold:
+            if counts[umi1] >= (counts[umi2] * 2) - 1:
+                adj_list[umi1].append(umi2)
+            if counts[umi2] >= (counts[umi1] * 2) - 1:
+                adj_list[umi2].append(umi1)
+    return adj_list
+
+def cluster_umis(umis):
+    """Cluster UMIs using UMI-tools directional method."""
+    if len(umis) == 1:
+        return umis
     
-    read_info = load_read_summary(args.read_summary)
-    tag_bam(args.input_bam, args.output_bam, read_info, args.threads, logger)
+    # Monkey-patch the UMI-tools clusterer
+    UMIClusterer._get_adj_list_directional = get_adj_list_directional_lev
+    
+    clusterer = UMIClusterer(cluster_method="directional")
+    umi_counts = collections.Counter(umis)
+    clusters = clusterer(umi_counts, threshold=2)
+    
+    if len(clusters) == len(umis):
+        return umis
+    
+    # Create mapping of corrected UMIs
+    umi_map = {}
+    for clust in clusters:
+        if len(clust) > 1:
+            for umi in clust[1:]:
+                umi_map[umi] = clust[0]
+    
+    return [umi_map.get(umi, umi) for umi in umis]
 
 def load_read_summary(file_path):
-    read_info = {}
-    with open(file_path, 'r') as f:
-        reader = csv.DictReader(f, delimiter='\t')
-        for row in reader:
-            read_info[row['read_id']] = row
-    return read_info
+    """Load and process read summary with UMI deduplication."""
+    # Read the TSV file
+    df = pd.read_csv(file_path, sep='\t')
+    
+    # Filter for reads with both corrected barcode and UMI
+    df = df[
+        (df['corrected_barcode'] != '-') & 
+        (df['corrected_umi'] != '-')
+    ]
+    
+    # Group by barcode and gene for UMI deduplication
+    df['gene_cell'] = df['gene'] + ':' + df['corrected_barcode']
+    grouped = df.groupby('gene_cell')
+    
+    # Perform UMI clustering for each group
+    for name, group in grouped:
+        umis = group['uncorrected_umi'].tolist()
+        corrected_umis = cluster_umis(umis)
+        df.loc[group.index, 'corrected_umi'] = corrected_umis
+    
+    # Convert to dictionary format
+    return df.set_index('read_id').to_dict('index')
 
 def tag_bam(input_bam, output_bam, read_info, threads, logger):
     with pysam.AlignmentFile(input_bam, "rb", threads=threads) as in_bam, \
          pysam.AlignmentFile(output_bam, "wb", template=in_bam, threads=threads) as out_bam:
         
         total_reads = 0
-        tagged_reads = 0
+        written_reads = 0
         skipped_reads = 0
         
         for read in in_bam:
             total_reads += 1
             read_id = read.query_name
+            
+            # Only process reads that are in our filtered read_info
             if read_id in read_info:
                 info = read_info[read_id]
                 
-                # Checking if barcode and UMI tags are present
-                if (info['corrected_barcode'] != '-' and info['corrected_umi'] != '-'):
-                    tagged_reads += 1
-                    
-                    # Settingtags only if they are present in the read_info
-                    if info['corrected_barcode'] != '-':
-                        read.set_tag("CB", info['corrected_barcode'], "Z")
-                    if info['uncorrected_barcode'] != '-':
-                        read.set_tag("CR", info['uncorrected_barcode'], "Z")
-                    if info['quality_barcode'] != '-':
-                        read.set_tag("CY", info['quality_barcode'], "Z")
-                    if info['corrected_umi'] != '-':
-                        read.set_tag("UB", info['corrected_umi'], "Z")
-                    if info['uncorrected_umi'] != '-':
-                        read.set_tag("UR", info['uncorrected_umi'], "Z")
-                    if info['quality_umi'] != '-':
-                        read.set_tag("UY", info['quality_umi'], "Z")
-                    if info['gene'] != '-':
-                        read.set_tag("GN", info['gene'], "Z")
-                    if info['transcript'] != '-':
-                        read.set_tag("TR", info['transcript'], "Z")
-                    
-                    # Adding transcriptome-specific tags
-                    read.set_tag("TS", read.reference_name, "Z")  # Transcript name
-                    read.set_tag("TL", read.reference_length, "i")  # Transcript length
-                    
-                    out_bam.write(read)
-                else:
-                    skipped_reads += 1
+                # Set all required tags
+                read.set_tag("CB", info['corrected_barcode'], "Z")
+                read.set_tag("CR", info['uncorrected_barcode'], "Z")
+                read.set_tag("CY", info.get('quality_barcode', '-'), "Z")
+                read.set_tag("UB", info['corrected_umi'], "Z")
+                read.set_tag("UR", info['uncorrected_umi'], "Z")
+                read.set_tag("UY", info.get('quality_umi', '-'), "Z")
+                read.set_tag("GN", info.get('gene', '-'), "Z")
+                read.set_tag("TR", info.get('transcript', '-'), "Z")
+                
+                out_bam.write(read)
+                written_reads += 1
             else:
                 skipped_reads += 1
 
     logger.info(f"Total reads processed: {total_reads}")
-    logger.info(f"Reads tagged and written: {tagged_reads} ({tagged_reads/total_reads:.2%})")
+    logger.info(f"Reads written: {written_reads} ({written_reads/total_reads:.2%})")
     logger.info(f"Reads skipped: {skipped_reads} ({skipped_reads/total_reads:.2%})")
+
+def main(args):
+    logger = get_named_logger("TagTranscriptomeBAM")
+    
+    logger.info("Loading and processing read summary with UMI deduplication")
+    read_info = load_read_summary(args.read_summary)
+    
+    logger.info("Tagging BAM file")
+    tag_bam(args.input_bam, args.output_bam, read_info, args.threads, logger)
