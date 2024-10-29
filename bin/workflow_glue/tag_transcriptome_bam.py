92d58b8b8e2b92b308efc2878e6532c15460ee9d
Dev Raj Bhattarai <drbhatta@c0175.cm.cluster>
2024-10-29 13:08:26 -0500

    Added tag_transcriptome_bam.py for tagging and UMI deduplication

--- bin/workflow_glue/tag_transcriptome_bam.py
+++ bin/workflow_glue/tag_transcriptome_bam.py
@@ -5,7 +5,6 @@ import itertools
 from pathlib import Path
 import pysam
 import pandas as pd
-import numpy as np
 from editdistance import eval as edit_distance
 from umi_tools import UMIClusterer
 from .util import get_named_logger, wf_parser
@@ -16,12 +15,9 @@ def argparser():
     parser.add_argument("output_bam", help="Output tagged BAM file")
     parser.add_argument("read_summary", help="read_summary.tsv file")
     parser.add_argument("--threads", type=int, default=1, help="Number of threads to use")
-    parser.add_argument(
-        "--ref_interval", type=int, default=1000,
-        help="Size of genomic window (bp) to assign as gene name if no gene assigned")
     return parser
 
-def get_adj_list_directional_lev(self, umis, counts, threshold=2):
+def get_adj_list_directional_lev(umis, counts, threshold=2):
     """Use Levenshtein distance for UMI clustering instead of hamming."""
     adj_list = {umi: [] for umi in umis}
     iter_umi_pairs = itertools.combinations(umis, 2)
@@ -33,134 +29,85 @@ def get_adj_list_directional_lev(self, umis, counts, threshold=2):
                 adj_list[umi2].append(umi1)
     return adj_list
 
-def umi_clusterer_call(self, umis, threshold):
-    """Modified UMIClusterer.__call__ to handle variable length UMIs."""
-    counts = umis
-    umis = list(umis.keys())
-
-    self.positions += 1
-    number_of_umis = len(umis)
-    self.total_umis_per_position += number_of_umis
-
-    if number_of_umis > self.max_umis_per_position:
-        self.max_umis_per_position = number_of_umis
-
-    adj_list = self.get_adj_list(umis, counts, threshold)
-    clusters = self.get_connected_components(umis, adj_list, counts)
-    final_umis = [list(x) for x in self.get_groups(clusters, adj_list, counts)]
-
-    return final_umis
-
-def create_region_name(chr_name, start, end, ref_interval):
-    """Create a fake gene name from alignment coordinates."""
-    midpoint = int((start + end) / 2)
-    interval_start = int(np.floor(midpoint / ref_interval) * ref_interval)
-    interval_end = int(np.ceil(midpoint / ref_interval) * ref_interval)
-    return f"{chr_name}_{interval_start}_{interval_end}"
-
-def cluster(umis):
-    """Cluster UMIs exactly as in create_matrix."""
-    if len(umis) == 1:  # early return
+def cluster_umis(umis):
+    """Cluster UMIs using directional method with Levenshtein distance."""
+    if len(umis) == 1:
         return umis
-    
-    # Monkey-patch the UMI-tools clusterer
-    UMIClusterer._get_adj_list_directional = get_adj_list_directional_lev
-    UMIClusterer.__call__ = umi_clusterer_call
     
     clusterer = UMIClusterer(cluster_method="directional")
+    clusterer._get_adj_list_directional = get_adj_list_directional_lev
+    
     umi_counts = collections.Counter(umis)
     clusters = clusterer(umi_counts, threshold=2)
-
-    if len(clusters) == len(umis):  # no corrections
+    
+    if len(clusters) == len(umis):
         return umis
+    
+    # Create mapping of corrected UMIs
+    umi_map = {}
+    for cluster in clusters:
+        if len(cluster) > 1:
+            for umi in cluster[1:]:
+                umi_map[umi] = cluster[0]
+    
+    return pd.Series(umis).replace(umi_map) if umi_map else umis
 
-    # create list of corrections
-    umi_map = dict()
-    for clust in clusters:
-        if len(clust) > 1:
-            for umi in clust[1:]:
-                umi_map[umi] = clust[0]
-                
-    if len(umi_map) > 0:
-        umis = pd.Series(umis).replace(umi_map)
-    return umis
+def deduplicate_and_tag_bam(input_bam, output_bam, read_info, threads, logger):
+    """Process BAM file with UMI deduplication and tagging."""
+    # First pass: group reads by barcode-gene-transcript combination and deduplicate UMIs
+    read_groups = collections.defaultdict(list)
+    
+    with pysam.AlignmentFile(input_bam, "rb", threads=threads) as in_bam:
+        for read in in_bam:
+            read_id = read.query_name
+            if read_id in read_info:
+                info = read_info[read_id]
+                if info['corrected_barcode'] != '-' and info['corrected_umi'] != '-':
+                    key = (
+                        info['corrected_barcode'],
+                        info.get('gene', '-'),
+                        info.get('transcript', '-')
+                    )
+                    read_groups[key].append((read_id, info['corrected_umi']))
 
-def process_reads(input_bam, read_info, ref_interval):
-    """Process reads to match create_matrix logic."""
-    # Create dataframe from read info
-    df = pd.DataFrame.from_dict(read_info, orient='index')
-    
-    # Handle unassigned genes
-    df['no_gene'] = False
-    df_no_gene = df.loc[df.gene == '-']
-    if len(df_no_gene) > 0:
-        # Create temporary gene names based on location
-        regions = df_no_gene.apply(
-            lambda x: create_region_name(x.chr, x.start, x.end, ref_interval), 
-            axis=1)
-        df.loc[regions.index, 'gene'] = regions
-        df.loc[df.index.isin(regions.index), 'no_gene'] = True
+    # Perform UMI deduplication within each group
+    kept_reads = set()
+    for group_reads in read_groups.values():
+        read_ids, umis = zip(*group_reads)
+        deduplicated_umis = cluster_umis(umis)
+        # Keep only the first occurrence of each UMI after deduplication
+        seen_umis = set()
+        for read_id, umi in zip(read_ids, deduplicated_umis):
+            if umi not in seen_umis:
+                kept_reads.add(read_id)
+                seen_umis.add(umi)
 
-    # Create gene/cell index for clustering
-    df["gene_cell"] = df["gene"] + ":" + df["corrected_barcode"]
-    df['read_id'] = df.index
-    df.set_index('gene_cell', inplace=True, drop=True)
-    
-    # Cluster UMIs
-    groups = df.groupby("gene_cell")["uncorrected_umi"]
-    df["corrected_umi"] = groups.transform(cluster)
-    df.set_index('read_id', drop=True, inplace=True)
-    
-    # Reset unassigned genes to '-'
-    df.loc[df.no_gene, 'gene'] = '-'
-    
-    return df
-
-def deduplicate_and_tag_bam(input_bam, output_bam, read_info, ref_interval, threads, logger):
-    """Process BAM file with UMI deduplication matching create_matrix."""
-    # Process reads using create_matrix logic
-    processed_df = process_reads(input_bam, read_info, ref_interval)
-    
-    # Create set of unique combinations after UMI clustering
-    unique_combinations = set(
-        zip(processed_df['corrected_barcode'], 
-            processed_df['gene'],
-            processed_df['corrected_umi']))
-    
-    # Write deduplicated and tagged BAM
+    # Second pass: write deduplicated and tagged BAM
     with pysam.AlignmentFile(input_bam, "rb", threads=threads) as in_bam, \
          pysam.AlignmentFile(output_bam, "wb", template=in_bam, threads=threads) as out_bam:
         
         total_reads = 0
         written_reads = 0
-        seen_combinations = set()
         
         for read in in_bam:
             total_reads += 1
             read_id = read.query_name
             
-            if read_id in processed_df.index:
-                info = processed_df.loc[read_id]
-                combination = (info['corrected_barcode'], 
-                             info['gene'],
-                             info['corrected_umi'])
+            if read_id in kept_reads:
+                info = read_info[read_id]
                 
-                # Only write first occurrence of each unique combination
-                if combination in unique_combinations and combination not in seen_combinations:
-                    seen_combinations.add(combination)
-                    
-                    # Set required tags
-                    read.set_tag("CB", info.get('corrected_barcode', '-'), "Z")
-                    read.set_tag("CR", info.get('uncorrected_barcode', '-'), "Z")
-                    read.set_tag("CY", info.get('quality_barcode', '-'), "Z")
-                    read.set_tag("UB", info.get('corrected_umi', '-'), "Z")
-                    read.set_tag("UR", info.get('uncorrected_umi', '-'), "Z")
-                    read.set_tag("UY", info.get('quality_umi', '-'), "Z")
-                    read.set_tag("GN", info.get('gene', '-'), "Z")
-                    read.set_tag("TR", info.get('transcript', '-'), "Z")
-                    
-                    out_bam.write(read)
-                    written_reads += 1
+                # Set required tags
+                read.set_tag("CB", info.get('corrected_barcode', '-'), "Z")
+                read.set_tag("CR", info.get('uncorrected_barcode', '-'), "Z")
+                read.set_tag("CY", info.get('quality_barcode', '-'), "Z")
+                read.set_tag("UB", info.get('corrected_umi', '-'), "Z")
+                read.set_tag("UR", info.get('uncorrected_umi', '-'), "Z")
+                read.set_tag("UY", info.get('quality_umi', '-'), "Z")
+                read.set_tag("GN", info.get('gene', '-'), "Z")
+                read.set_tag("TR", info.get('transcript', '-'), "Z")
+                
+                out_bam.write(read)
+                written_reads += 1
 
     logger.info(f"Total reads processed: {total_reads}")
     logger.info(f"Unique reads after deduplication: {written_reads} ({written_reads/total_reads:.2%})")
@@ -168,16 +115,13 @@ def deduplicate_and_tag_bam(input_bam, output_bam, read_info, ref_interval, thre
 def main(args):
     logger = get_named_logger("TagTranscriptomeBAM")
     
-    # Load read summary
+    # Load read summary information
+    logger.info("Loading read summary information")
     read_info = {}
-    df = pd.read_csv(args.read_summary, sep='\t')
-    read_info = df.set_index('read_id').to_dict('index')
+    with open(args.read_summary, 'r') as f:
+        for row in pd.read_csv(f, sep='\t').to_dict('records'):
+            read_info[row['read_id']] = row
     
     # Process BAM file
-    deduplicate_and_tag_bam(
-        args.input_bam, 
-        args.output_bam, 
-        read_info, 
-        args.ref_interval,
-        args.threads, 
-        logger)
+    logger.info("Processing BAM file")
+    deduplicate_and_tag_bam(args.input_bam, args.output_bam, read_info, args.threads, logger)
