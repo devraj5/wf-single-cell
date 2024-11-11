9c4493a428aa5e5c8a4db8ac3be446f6fe9fe003
Dev Raj Bhattarai <drbhatta@login004.cm.cluster>
2024-11-11 10:13:01 -0600

    added tag_transcriptome_bam.py and correct_umi.py

--- bin/workflow_glue/correct_umis.py
+++ bin/workflow_glue/correct_umis.py
@@ -5,7 +5,15 @@ import itertools
 import pysam
 from editdistance import eval as edit_distance
 from umi_tools import UMIClusterer
-import argparse
+from .util import get_named_logger, wf_parser  # Import from workflow-glue utils
+
+def argparser():
+    """Create argument parser."""
+    parser = wf_parser("correct_umis")
+    parser.add_argument("-i", "--input", required=True, help="Input BAM file")
+    parser.add_argument("-o", "--output", required=True, help="Output BAM file")
+    parser.add_argument("--threads", type=int, default=1, help="Number of threads for sorting")
+    return parser
 
 def get_adj_list_directional_lev(self, umis, counts, threshold=2):
     """Use Levenshtein distance for UMI clustering instead of hamming."""
@@ -48,39 +56,32 @@ def cluster_umis(umis):
 
 def process_bam(in_bam, out_bam):
     """Process BAM file to correct UMIs."""
+    logger = get_named_logger("UMICorrect")
+    logger.info(f"Processing BAM file: {in_bam}")
+    
     with pysam.AlignmentFile(in_bam, "rb") as input_bam:
-        # Create output BAM with same header
         with pysam.AlignmentFile(out_bam, "wb", header=input_bam.header) as output_bam:
-            # Group reads by cell barcode and gene
             current_group = []
             current_key = None
             
-            # Iterate through sorted BAM
             for read in input_bam.fetch(until_eof=True):
-                # Get cell barcode and gene
                 cell_barcode = read.get_tag("CB")
                 gene = read.get_tag("GN")
                 key = f"{cell_barcode}_{gene}"
                 
                 if key != current_key:
-                    # Process previous group
                     if current_group:
-                        # Get UMIs from group
                         umis = [read.get_tag("UB") for read in current_group]
-                        # Cluster UMIs
                         corrected_umi = cluster_umis(umis)
-                        # Write corrected reads
                         for read in current_group:
                             read.set_tag("UB", corrected_umi)
                             output_bam.write(read)
                     
-                    # Start new group
                     current_group = [read]
                     current_key = key
                 else:
                     current_group.append(read)
             
-            # Process final group
             if current_group:
                 umis = [read.get_tag("UB") for read in current_group]
                 corrected_umi = cluster_umis(umis)
@@ -88,13 +89,6 @@ def process_bam(in_bam, out_bam):
                     read.set_tag("UB", corrected_umi)
                     output_bam.write(read)
 
-def main():
-    parser = argparse.ArgumentParser(description="Correct UMIs in tagged BAM file")
-    parser.add_argument("-i", "--input", required=True, help="Input BAM file")
-    parser.add_argument("-o", "--output", required=True, help="Output BAM file")
-    args = parser.parse_args()
-
+def main(args):
+    """Entry point for correct_umis command."""
     process_bam(args.input, args.output)
-
-if __name__ == "__main__":
-    main()
